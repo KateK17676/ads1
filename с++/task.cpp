d43bd3ff49199c44ff58969710944d2a3850a8b2
KateK17676 <linv3929@gmail.com>
2026-09-10 23:52:36 +0300

    Update task.cpp
    Добавлены комментарии

--- с++/task.cpp
+++ с++/task.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     double l, r, b, s;
-    l = 0;
+    l = 0; // промежуток
     r = 1;
     s = 0;
     double fl, fb;
@@ -11,19 +11,18 @@ int main() {
     while (r - l > 0.0001)
     {
         s++;
-        b = (l + r) / 2;
+        b = (l + r) / 2; // середина
         fl = sqrt(1 - l) - tan(l);
         fb = sqrt(1 - b) - tan(b);
-        if (fl * fb < 0)
-    {
-        r = b;
-    }
-    else
-    {
-        l = b;
-    }
-    std::cout << s << " " << b << std::endl;
-
+        if (fl * fb < 0) // значения имеют разные знаки
+        {
+            r = b; // корень между l и b
+        }
+        else
+        {
+            l = b; // между b и r
+        }
+        std::cout << s << " " << b << std::endl;
     }
     b = (l + r) / 2;
     std::cout << "Корень: " << b << std::endl;
